7f84d4a6bebd3145e5d1a321180a94e6b0f82d85
larajakl <lara.jakl@gmx.at>
2024-04-30 11:12:17 +0200

    Lara Jakl Assessment 2 group B

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
 """
 Multiplication Table
 """
-# Provide your solution here
+
+def print_multiplication_table(x : int, y : int) -> int:
+    if x > 0 and y > 0:
+        i = 1
+        while i <= y:
+            print(f"{i} * {x} = {i * x}")
+            i += 1
+    else:
+        print("Number and rows cannot be les than 1")
 
 """
 Palindromes
 """
-# Provide your solution here
+
+
+def reverse(text: str) -> str:
+    text = text.lower  # to make all lowercase
+    text = str(text)
+    text = text.replace(" ", "")  # to omit all spaces
+    a = -1
+    reversed_text = ""
+    while a >= -len(text):
+        reversed_text += text[a]
+        a -= 1
+    return reversed_text
+
+
+def is_palindrome(text: str):
+    text = text.lower  # to make all lowercase
+    text = str(text)
+    text.replace(" ", "")  # to omit all spaces
+    x = True
+    y = False
+    if text == reverse(text):  # something does not work here but I could not figure it out
+        print(x)
+        return x
+    else:
+        print(y)
+        return y
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("Test your functions by calling them here. Use different parameter values to test them with different scenarios.")
-
-
+    is_palindrome("tami")
